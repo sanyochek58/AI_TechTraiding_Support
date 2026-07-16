6f96bd96366cce38870e6187b6b547e7354f3b89
Alexandr Burayev <sanya.burayev.03@mail.ru>
2026-07-17 02:59:47 +0300

    Обновление зависимости Jackson Gson и структуры проека

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,27 @@
 plugins {
     id("java")
+    application
 }
 
-group = "com.example"
+group = "com.example.aitraiding"
 version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+application {
+    mainClass.set("com.example.aitraiding.Main")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.0.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.19.1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
